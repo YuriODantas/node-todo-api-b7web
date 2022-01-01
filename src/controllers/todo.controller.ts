6586cb189f8a1428c9e1f9a62bbd5af797a70470
YuriODantas <iruy3343@gmail.com>
2022-01-01 20:24:57 -0300

    Cria função de GET all

--- src/controllers/todo.controller.ts
+++ src/controllers/todo.controller.ts
@@ -1,18 +1,19 @@
 import { Request, Response } from 'express';
 import { Todo } from '../models/Todo';
 
-export const all = async () => {
-    
-}
+export const all = async (req: Request, res: Response) => {
+  const list = await Todo.findAll();
+  res.status(200).json({ list });
+};
 
 export const add = async () => {
 
-}
+};
 
 export const update = async () => {
 
-}
+};
 
 export const remove = async () => {
 
-}
+};
